510e1f0bf1e8d039f72f4196de0cd91f1b938c7c
sebastian cardenas <scardenasga@unbosque.edu.co>
2026-04-21 21:46:16 -0500

    hotfix de la database. schema sql

--- backend/src/test/java/co/edu/unbosque/backend/repository/VentaRepositoryTest.java
+++ backend/src/test/java/co/edu/unbosque/backend/repository/VentaRepositoryTest.java
@@ -4,7 +4,7 @@ import co.edu.unbosque.backend.model.entity.Usuario;
 import co.edu.unbosque.backend.model.entity.Venta;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
